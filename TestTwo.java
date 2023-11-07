766a4387c6adfa0a5f285499b59be6a041512392
Chris Castro <cmc-castro@hotmail.com>
2023-11-06 22:23:54 -0500

    Initial commit for test case 2

---
+++ TestTwo.java
@@ -0,0 +1,10 @@
+// Test1.java
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestTwo {
+    @Test
+    public void testCase1() {
+        assertEquals(1, LongestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring("bbbbb"));
+    }
+}
